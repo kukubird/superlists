1faf34e2ef797cd13950389fd465eeccfc5f76d2
Anita <anita@anita-ubuntu.(none)>
2013-12-18 14:00:47 +0300

    Adjusted our models

--- lists/tests.py
+++ lists/tests.py
@@ -10,7 +10,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -25,23 +25,32 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html) #we use the decode to covert the response. content bytes into a Python unicode string, which allows us to comapre strings with strings.
     
 class ItemModelTest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
 
-    def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.save()
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
+		first_item.save()
 
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.save()
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.list = list_
+		second_item.save()
 
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
+		saved_lists = List.objects.all()
+		self.assertEqual(saved_lists.count(), 1)
+		self.assertEqual(saved_lists[0], list_)
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
 
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)
+		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
@@ -50,8 +59,9 @@ class ListViewTest(TestCase):
 		self.assertTemplateUsed(response, 'list.html')
 
 	def test_displays_all_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
+		list_ = List.objects.create()
+		Item.objects.create(text='itemey 1', list=list_)
+		Item.objects.create(text='itemey 2', list=list_)
 
 		response = self.client.get('/lists/the-only-list-in-the-world/') #1
 
@@ -75,3 +85,31 @@ class NewListTest(TestCase):
             data={'item_text': 'A new list item'}
         )
 		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
+class ListAndItemModelsTest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+			
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.list = list_
+		second_item.save()
+
+		saved_lists = List.objects.all()
+		self.assertEqual(saved_lists.count(), 1)
+		self.assertEqual(saved_lists[0], list_)
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)
+		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_)
